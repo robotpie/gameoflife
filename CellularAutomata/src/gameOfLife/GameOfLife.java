df8593f28be507dce808cc491fb93c2111d68f06
mrkyrmon <marek@cse.yorku.ca>
2014-05-21 11:46:08 -0400

    initial commit

---
+++ CellularAutomata/src/gameOfLife/GameOfLife.java
@@ -0,0 +1,40 @@
+package gameOfLife;
+/*
+ * should this class be immutable?
+ * memory vs. time vs. parallelism considerations
+ * 
+ */
+
+public class GameOfLife {
+private int lattice[][];
+private int rows;
+private int columns;
+	public GameOfLife(int rows, int columns){
+		if(rows <= 0 || columns <= 0)
+		{
+			throw new IllegalArgumentException("rows and columns must both be nonzero") ;
+		}
+		lattice = new int[rows][columns];//assume Java initializes to 0
+		this.rows = rows;
+		this.columns = columns;
+	};
+	
+	public int getState(int row, int column)
+	{
+		return lattice[row][column];
+	}
+	
+	public String toString()
+	{
+		String result = "";
+		for(int row = 0; row < rows; row++)
+		{
+			for(int column = 0; column < columns; column++)
+			{
+				result += getState(row, column);
+			}
+			result += "\n";
+		}
+		return result;
+	}
+}
---
+++ CellularAutomata/src/tests/CreationTest.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+import gameOfLife.GameOfLife;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author marek
+ *
+ */
+public class CreationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreation() {
+		GameOfLife gol = new GameOfLife(10, 10);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testZeroSizeCreation() {
+		GameOfLife gol = new GameOfLife(0, 0);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNegativeSizeCreation() {
+		GameOfLife gol = new GameOfLife(-1, -1);
+	}
+	
+	@Test
+	public void testAccessCorners() {
+		GameOfLife gol = new GameOfLife(10, 10);
+		assertEquals(0, gol.getState(0,0));
+		assertEquals(0, gol.getState(9,9));
+		assertEquals(0, gol.getState(0,9));
+		assertEquals(0, gol.getState(9,0));
+	}
+	
+	@Test (expected = ArrayIndexOutOfBoundsException.class)
+	public void testAccessOutOfBounds() {
+		GameOfLife gol = new GameOfLife(10, 10);
+		int x = gol.getState(10, 10);
+	}
+	
+	@Test
+	public void testToString() {
+		GameOfLife gol = new GameOfLife(3, 3);
+		assertEquals("000\n000\n000\n", gol.toString());
+	}
+	
+	
+
+}
